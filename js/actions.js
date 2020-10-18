cef0c5aa8ee7a8bee0fd2346616483edd47f829d
zanehuggins <54915657+zanehuggins@users.noreply.github.com>
2020-10-18 14:21:57 -0400

    .

--- js/actions.js
+++ js/actions.js
@@ -17,11 +17,50 @@
 
 // Portfolio actions
 
-    function toggleDisplay2() {
-      var element = document.getElementById("item-1");
-      element.classList.toggle("item-display");
-    }
+// Item 1 Functions
+function toggleDisplay2() {
+  var x = document.getElementById("item-1");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
 
+
+    function toggleOpacity1() {
+      var element = document.getElementById("item-2-icon");
+      element.classList.toggle("opacity");
+
+      var element = document.getElementById("item-3-icon");
+      element.classList.toggle("opacity");
+
+
+          var element = document.getElementById("item-4-icon");
+          element.classList.toggle("opacity");
+
+
+        var element = document.getElementById("item-5-icon");
+        element.classList.toggle("opacity");
+
+
+      var element = document.getElementById("item-6-icon");
+      element.classList.toggle("opacity");
+
+
+      var element = document.getElementById("item-7-icon");
+      element.classList.toggle("opacity");
+
+
+      var element = document.getElementById("item-8-icon");
+      element.classList.toggle("opacity");
+      }
+
+
+
+
+
+// Item 2 Functions
     function toggleDisplay3() {
       var x = document.getElementById("item-2");
       if (x.style.display === "block") {
