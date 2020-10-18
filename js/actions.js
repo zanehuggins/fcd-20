04a4d7aacdff730fbce7949759b228916f3cbfc7
zanehuggins <54915657+zanehuggins@users.noreply.github.com>
2020-10-18 19:46:57 -0400

    .

--- js/actions.js
+++ js/actions.js
@@ -18,7 +18,7 @@
 // Portfolio actions
 
 // Item 1 Functions
-function toggleDisplay2() {
+function toggleDisplay1() {
   var x = document.getElementById("item-1");
   if (x.style.display === "block") {
     x.style.display = "none";
@@ -57,11 +57,23 @@ function toggleDisplay2() {
       }
 
 
+      function move() {
+        var element = document.getElementById("item-1-icon");
+        element.classList.toggle("move");
+      }
 
+      function closeIcon() {
+        var x = document.getElementById("close");
+        if (x.style.display === "block") {
+          x.style.display = "none";
+        } else {
+          x.style.display = "block";
+        }
+      }
 
 
 // Item 2 Functions
-    function toggleDisplay3() {
+    function toggleDisplay2() {
       var x = document.getElementById("item-2");
       if (x.style.display === "block") {
         x.style.display = "none";
@@ -70,18 +82,45 @@ function toggleDisplay2() {
       }
     }
 
+    function toggleOpacity2() {
+      var element = document.getElementById("item-1-icon");
+      element.classList.toggle("opacity");
+
+      var element = document.getElementById("item-3-icon");
+      element.classList.toggle("opacity");
 
 
-function move() {
-  var element = document.getElementById("item-1-icon");
-  element.classList.toggle("move");
-}
+          var element = document.getElementById("item-4-icon");
+          element.classList.toggle("opacity");
 
-    function closeIcon() {
-      var x = document.getElementById("close");
-      if (x.style.display === "block") {
-        x.style.display = "none";
-      } else {
-        x.style.display = "block";
+
+        var element = document.getElementById("item-5-icon");
+        element.classList.toggle("opacity");
+
+
+      var element = document.getElementById("item-6-icon");
+      element.classList.toggle("opacity");
+
+
+      var element = document.getElementById("item-7-icon");
+      element.classList.toggle("opacity");
+
+
+      var element = document.getElementById("item-8-icon");
+      element.classList.toggle("opacity");
       }
-    }
+
+
+      function move2() {
+        var element = document.getElementById("item-2-icon");
+        element.classList.toggle("move2");
+      }
+
+      function closeIcon2() {
+        var x = document.getElementById("close2");
+        if (x.style.display === "block") {
+          x.style.display = "none";
+        } else {
+          x.style.display = "block";
+        }
+      }
