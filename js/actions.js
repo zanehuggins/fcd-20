2cd54f0b90847bcc160e3b0a78a5555448312f1b
zanehuggins <54915657+zanehuggins@users.noreply.github.com>
2020-10-18 20:59:23 -0400

    .

--- js/actions.js
+++ js/actions.js
@@ -124,3 +124,166 @@ function toggleDisplay1() {
           x.style.display = "block";
         }
       }
+
+
+      // Item 3 Functions
+          function toggleDisplay3() {
+            var x = document.getElementById("item-3");
+            if (x.style.display === "block") {
+              x.style.display = "none";
+            } else {
+              x.style.display = "block";
+            }
+          }
+
+          function toggleOpacity3() {
+            var element = document.getElementById("item-1-icon");
+            element.classList.toggle("opacity");
+
+            var element = document.getElementById("item-2-icon");
+            element.classList.toggle("opacity");
+
+
+                var element = document.getElementById("item-4-icon");
+                element.classList.toggle("opacity");
+
+
+              var element = document.getElementById("item-5-icon");
+              element.classList.toggle("opacity");
+
+
+            var element = document.getElementById("item-6-icon");
+            element.classList.toggle("opacity");
+
+
+            var element = document.getElementById("item-7-icon");
+            element.classList.toggle("opacity");
+
+
+            var element = document.getElementById("item-8-icon");
+            element.classList.toggle("opacity");
+            }
+
+
+            function move3() {
+              var element = document.getElementById("item-3-icon");
+              element.classList.toggle("move3");
+            }
+
+            function closeIcon3() {
+              var x = document.getElementById("close3");
+              if (x.style.display === "block") {
+                x.style.display = "none";
+              } else {
+                x.style.display = "block";
+              }
+            }
+
+
+            // Item 4 Functions
+                function toggleDisplay4() {
+                  var x = document.getElementById("item-4");
+                  if (x.style.display === "block") {
+                    x.style.display = "none";
+                  } else {
+                    x.style.display = "block";
+                  }
+                }
+
+                function toggleOpacity4() {
+                  var element = document.getElementById("item-1-icon");
+                  element.classList.toggle("opacity");
+
+                  var element = document.getElementById("item-2-icon");
+                  element.classList.toggle("opacity");
+
+
+                      var element = document.getElementById("item-3-icon");
+                      element.classList.toggle("opacity");
+
+
+                    var element = document.getElementById("item-5-icon");
+                    element.classList.toggle("opacity");
+
+
+                  var element = document.getElementById("item-6-icon");
+                  element.classList.toggle("opacity");
+
+
+                  var element = document.getElementById("item-7-icon");
+                  element.classList.toggle("opacity");
+
+
+                  var element = document.getElementById("item-8-icon");
+                  element.classList.toggle("opacity");
+                  }
+
+
+                  function move4() {
+                    var element = document.getElementById("item-4-icon");
+                    element.classList.toggle("move4");
+                  }
+
+                  function closeIcon4() {
+                    var x = document.getElementById("close4");
+                    if (x.style.display === "block") {
+                      x.style.display = "none";
+                    } else {
+                      x.style.display = "block";
+                    }
+                  }
+
+
+
+                  // Item 5 Functions
+                      function toggleDisplay5() {
+                        var x = document.getElementById("item-5");
+                        if (x.style.display === "block") {
+                          x.style.display = "none";
+                        } else {
+                          x.style.display = "block";
+                        }
+                      }
+
+                      function toggleOpacity4() {
+                        var element = document.getElementById("item-1-icon");
+                        element.classList.toggle("opacity");
+
+                        var element = document.getElementById("item-2-icon");
+                        element.classList.toggle("opacity");
+
+
+                            var element = document.getElementById("item-3-icon");
+                            element.classList.toggle("opacity");
+
+
+                          var element = document.getElementById("item-5-icon");
+                          element.classList.toggle("opacity");
+
+
+                        var element = document.getElementById("item-6-icon");
+                        element.classList.toggle("opacity");
+
+
+                        var element = document.getElementById("item-7-icon");
+                        element.classList.toggle("opacity");
+
+
+                        var element = document.getElementById("item-8-icon");
+                        element.classList.toggle("opacity");
+                        }
+
+
+                        function move5() {
+                          var element = document.getElementById("item-5-icon");
+                          element.classList.toggle("move5");
+                        }
+
+                        function closeIcon5() {
+                          var x = document.getElementById("close5");
+                          if (x.style.display === "block") {
+                            x.style.display = "none";
+                          } else {
+                            x.style.display = "block";
+                          }
+                        }
